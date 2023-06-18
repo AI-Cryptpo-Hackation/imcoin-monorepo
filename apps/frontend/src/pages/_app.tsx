c5d8ff01eaebd25dbb2ab7fe2162b56e678e3405
serinuntius <wasab1gj@gmail.com>
2023-06-18 21:56:49 +0900

    Rainbow kitをインスコ

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -2,8 +2,39 @@ import "@/styles/globals.css";
 import '@rainbow-me/rainbowkit/styles.css';
 
 import "@charcoal-ui/icons";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
 import type { AppProps } from "next/app";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { arbitrum, mainnet, optimism, polygon } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: '汎用人格搭載電子通貨「I’m Coin」',
+  projectId: '878e94e3fb33a18974bfe8fe80521f54',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+
 }
