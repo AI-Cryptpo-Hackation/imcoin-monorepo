084e3ac120fc93d9eb81065557d91519a4ec29da
serinuntius <wasab1gj@gmail.com>
2023-06-18 21:40:22 +0900

    rainbowのcssあてる

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@rainbow-me/rainbowkit/styles.css';
+
 import "@charcoal-ui/icons";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
