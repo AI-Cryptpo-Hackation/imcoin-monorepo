158cf5d48e1b1f19a34d13c16e5f2194426b1cf8
Inaridiy <inainainariinainari@gmail.com>
2023-06-18 11:34:32 +0900

    simple socket io

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,15 +1,21 @@
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 import { setupHyperObjects, SQLiteStore } from "@inaridiy/hyper-objects";
 import dotenv from "dotenv";
 import { Hono } from "hono";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 await setupHyperObjects(new SQLiteStore());
-
 const app = new Hono();
+const server = serve(app);
+const io = new Server(server);
 
+app.use("/static/*", serveStatic({ root: "./" }));
 app.get("/", (c) => c.text("Hello Node.js!"));
 app.all("/teapot", (c) => c.text("I'm a teapot!", 418));
 
-serve(app, (info) => console.info(`Server started on port ${info.port}`));
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
