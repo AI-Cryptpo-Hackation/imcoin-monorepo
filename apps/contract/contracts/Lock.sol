dd7d065497147d3e3005e9bfcb9a9f6855891a64
Inaridiy <inainainariinainari@gmail.com>
2023-06-19 21:49:39 +0900

    crazy token

--- apps/contract/contracts/Lock.sol
+++ apps/contract/contracts/Lock.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract IMCToken is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit {
+    uint256 public scalar = 10 ** 8; // 1 IMC = 10 ** decimal / scalar
+
     constructor() ERC20("I'm Coin.", "IMC") ERC20Permit("I'm Coin.") {}
 
     function pause() public onlyOwner {
@@ -19,7 +21,18 @@ contract IMCToken is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit {
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+        uint amount_ = amount * scalar;
+        _mint(to, amount_);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        uint amount_ = amount * scalar;
+        _burn(from, amount_);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        uint256 amount = super.balanceOf(account);
+        return amount / scalar;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
@@ -27,6 +40,7 @@ contract IMCToken is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit {
         whenNotPaused
         override
     {
-        super._beforeTokenTransfer(from, to, amount);
+        uint amount_ = amount * scalar;
+        super._beforeTokenTransfer(from, to, amount_);
     }
 }
