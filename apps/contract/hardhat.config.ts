d9129ea508711c3e5399f9e3a9a6b8f24b3c42d3
Inaridiy <inainainariinainari@gmail.com>
2023-06-20 00:02:20 +0900

    deploy

--- apps/contract/hardhat.config.ts
+++ apps/contract/hardhat.config.ts
@@ -1,8 +1,27 @@
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 
+dotenv.config();
+
+if (!process.env.PRIVATE_KEY) throw new Error("PRIVATE_KEY is not defined");
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
--- apps/contract/scripts/deploy.ts
+++ apps/contract/scripts/deploy.ts
@@ -1,23 +1,16 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const IMCToken = await ethers.getContractFactory("IMCToken");
+  const imctoken = await IMCToken.deploy(
+    "0x1810406E4A76Ba98AaFa399CF860B1FAA8FF0A21"
   );
+
+  await imctoken.deployed();
+
+  console.log("IMCToken deployed to:", imctoken.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
