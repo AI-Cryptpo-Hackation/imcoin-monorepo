01f9c387197fc307f3d539e43d37d415633dd919
serinuntius <wasab1gj@gmail.com>
2023-06-20 00:04:22 +0900

    CORSの設定をしてるけど、うまく行ってない

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,7 @@ import { serveStatic } from "@hono/node-server/serve-static";
 import { setupHyperObjects, SQLiteStore } from "@inaridiy/hyper-objects";
 import dotenv from "dotenv";
 import { Hono } from "hono";
+import { cors } from 'hono/cors';
 import { Server } from "socket.io";
 import { Comment, Liver } from "./models";
 
@@ -10,14 +11,23 @@ dotenv.config();
 
 await setupHyperObjects(new SQLiteStore("./db.sqlite"));
 const app = new Hono();
-const server = serve(app);
+const server = serve({
+  fetch: app.fetch,
+  port: 3001,
+});
 const io = new Server(server);
+console.log("Server started at http://localhost:3001 (default)")
 
+// TODO: CORSの設定が全く効かない！！！！なんで！！！！
+app.use('/*', cors({
+  origin: 'http://localhost:3000',
+}));
 app.use("/static/*", serveStatic({ root: "./" }));
 app.get("/", (c) => c.text("Hello Node.js!"));
 app.all("/teapot", (c) => c.text("I'm a teapot!", 418));
 
 io.on("connection", async (socket) => {
+  console.log('a user connected');
   socket.emit("history", await Comment.getHistorySnapshot());
   socket.on("send-comment", async (data) => {
     await Comment.send(data);
