4445be510f873f056162869fdf5e21c4a5cab5cb
serinuntius <wasab1gj@gmail.com>
2023-06-20 00:04:36 +0900

    こんな感じで動くはず

--- apps/frontend/src/components/commentList.tsx
+++ apps/frontend/src/components/commentList.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 
-type Comment = {
-  id: number;
+export type Comment = {
+  address: string;
   text: string;
-  author: string;
+  username: string;
   profileImageUrl: string;
+  createdAt: number;
 }
 
 type Props = {
@@ -17,11 +18,11 @@ export const CommentList: FC<Props> = ({ comments }) => {
     <div className="absolute right-16 mt-[80px] w-[462px] overflow-y-auto max-h-[450px]">
       <div className="mx-auto max-w-4xl w-full p-16 flex-wrap bg-black bg-opacity-60">
 
-        {comments.map((comment) => (
-          <div key={comment.id} className="py-4">
+        {comments.map((comment, index) => (
+          <div key={index} className="py-4">
             <div className="flex flex-row w-[462px]">
-              <img src={comment.profileImageUrl} alt={comment.author} className="bg-white object-cover h-24 w-24 rounded-[32px] m-4"/>
-              <span className="px-4 text-white break-words typography-4 font-M_PLUS_2 whitespace-pre-wrap overflow-wrap-break-word">{comment.author}: {comment.text}</span>
+              <img src={comment.profileImageUrl} alt={comment.username} className="bg-white object-cover h-24 w-24 rounded-[32px] m-4"/>
+              <span className="px-4 text-white break-words typography-4 font-M_PLUS_2 whitespace-pre-wrap overflow-wrap-break-word">{comment.username}: {comment.text}</span>
             </div>
           </div>
         ))}
